c70c3e8dab77da61351d8faa817178019da984fa
Andrey Sitnikov <andreys@lavabit.com>
2020-11-11 11:58:42 +0300

    Boi

--- main.py
+++ main.py
@@ -6,22 +6,72 @@ import subprocess
 import logging
 import sys
 import logging
+import sqlite3
+from webbrowser import open as openurl
 
-try: from logger import Logger
+# Custom Logger
+try:
+    from logger import Logger
+
+    logging.setLoggerClass(Logger)
 except ImportError:
-    sys.exit(
-        "Cannot locate one or more necessary files.\n" + 
-        "Make sure that you are running the script from the right folder."
-    )
+    print("Warning: cannot load file 'logging.py'. Logging will lack color.")
+
+log = logging.getLogger("Aerial Main")
 
 # Version-specific Python command, avoids situations where the default
 # Python version is different from the one used to run the script.
 python = "python" + str(sys.version_info[0]) + "." + str(sys.version_info[1])
 
 # Installing & Upgrading Requirements
+log.info("$CYANUpdating Packages...")
 subprocess.check_call(
-    [python, "-m", "pip", "install", "-U", "pip", "fortnitepy", "websockets"],
-    stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+    [
+        python,
+        "-m",
+        "pip",
+        "install",
+        "-U",
+        "pip",
+        "fortnitepy",
+        "websockets",
+        "prompt_toolkit",
+    ],
+    stdout=subprocess.DEVNULL,
+    stderr=subprocess.DEVNULL,
 )
 
-# TBD: literally everything else, im watching the apple event rn lol
+import aiohttp
+import fortnitepy
+import prompt_toolkit
+
+# Prompt Setup
+async def prompt():
+    session = prompt_toolkit.PromptSession()
+    while True:
+        with prompt_toolkit.patch_stdout():
+            try:
+                cmd = await session.prompt_async("> ")
+            except KeyboardInterrupt:
+                continue
+            except EOFError:
+                break
+
+
+# Database
+db = sqlite3.connect("data.sqlite")
+c = db.cursor()
+c.execute(
+    """SELECT `name` FROM `sqlite_master` WHERE TYPE = 'table' AND NAME = 'accounts';"""
+)
+if not c.fetchone():
+    c.execute(
+        """CREATE TABLE "accounts" (
+              "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+              "email" varchar(255) NOT NULL,
+              "password" varchar(255) NOT NULL,
+              "device_id" varchar(255) NOT NULL,
+              "account_id" varchar(255) NOT NULL,
+              "secret" varchar(255) NOT NULL
+            );"""
+    )
